b853939bca11eea73c1b10b889482ef33e3b3a19
mortdur <121159231+mortdur@users.noreply.github.com>
2023-02-14 09:40:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 class TwitterSpider(scrapy.Spider):
     name = "twitter"
@@ -9,15 +10,19 @@ class TwitterSpider(scrapy.Spider):
 
     def parse(self, response):
         for tweet in response.css('div.tweet'):
-            yield {
+            print({
                 'username': tweet.css('span.username::text').get(),
                 'tweet': tweet.css('p.tweet-text::text').get(),
                 'time': tweet.css('span._timestamp::attr(data-time)').get()
-            }
-        
+            })
+
         next_page = response.css('div.stream-container div.stream > div.stream-item:last-child > div.stream-item > div.tweet:last-child > div.tweet-context a.js-next-link::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+
+process = CrawlerProcess()
+process.crawl(TwitterSpider)
+process.start()
 
 tab1, tab2= st.tabs(["Una entrada","ya"])
 with tab1:

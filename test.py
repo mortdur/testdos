117af550427f92b7e2b24981425e6143b8d6756d
mortdur <121159231+mortdur@users.noreply.github.com>
2023-02-14 10:40:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,32 +2,21 @@ import streamlit as st
 import streamlit.components.v1 as components
 import requests
 class Tweet(object):
-	def __init__(self, tid, embed_str=False):
-		if not embed_str:
-			try:
-				# Use Twitter's oEmbed API
-				# https://dev.twitter.com/web/embedded-tweets
-				api = 'https://publish.twitter.com/oembed?url=https://twitter.com/XXX/status/'+tid
-				response = requests.get(api)
-				self.text = response.json()["html"]
-			except:
-				return "<blockquote class='missing'>This tweet is no longer available.</blockquote>"
-		else:
-			self.text = tid
+    def __init__(self, s, embed_str=False):
+        if not embed_str:
+            # Use Twitter's oEmbed API
+            # https://dev.twitter.com/web/embedded-tweets
+            api = "https://publish.twitter.com/oembed?url={}".format(s)
+            response = requests.get(api)
+            self.text = response.json()["html"]
+        else:
+            self.text = s
 
-	def _repr_html_(self):
-		return self.text
+    def _repr_html_(self):
+        return self.text
 
-	def component(self):
-		return components.html(self.text, height=600)
+    def component(self):
+        return components.html(self.text, height=600)
 
-def top_daily_tweets(df):
-	df = df.sort_values(['Followers'], ascending=False).head(10)
-	return df
 
-top_daily_tweets = top_daily_tweets(data)
-
-with st.expander("Twitter feed", expanded=True):             
-	st.subheader("Most influential tweets")
-	for i in range(len(top_daily_tweets)):
-		t = Tweet(top_daily_tweets.iloc[i]['tweet_id']).component()
+t = Tweet("https://twitter.com/OReillyMedia/status/901048172738482176").component()

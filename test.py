642a6f84b3bd1c908321d7eca831f41ce7d04e73
mortdur <121159231+mortdur@users.noreply.github.com>
2023-02-14 10:14:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,5 @@
 import streamlit as st
-import scrapy
-from scrapy.crawler import CrawlerProcess
-
-class TwitterSpider(scrapy.Spider):
-    name = "twitter"
-    start_urls = [
-        'https://twitter.com/search?q=%23yourhashtag&src=typd'
-    ]
-
-    def parse(self, response):
-        for tweet in response.css('div.tweet'):
-            print({
-                'username': tweet.css('span.username::text').get(),
-                'tweet': tweet.css('p.tweet-text::text').get(),
-                'time': tweet.css('span._timestamp::attr(data-time)').get()
-            })
-
-        next_page = response.css('div.stream-container div.stream > div.stream-item:last-child > div.stream-item > div.tweet:last-child > div.tweet-context a.js-next-link::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
-
-process = CrawlerProcess()
-process.crawl(TwitterSpider)
-process.start()
-
+<a href="https://twitter.com/intent/tweet?button_hashtag=LoveTwitter&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #LoveTwitter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 tab1, tab2= st.tabs(["Una entrada","ya"])
 with tab1:
     st.slider("j",0.0, 5.0,key="x0")

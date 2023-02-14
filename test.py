b572f129f1a5230d5fa719459a9239b1e58e20b1
mortdur <121159231+mortdur@users.noreply.github.com>
2023-02-14 10:17:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
 import streamlit as st
 <a href="https://twitter.com/intent/tweet?button_hashtag=LoveTwitter&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #LoveTwitter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-tab1, tab2= st.tabs(["Una entrada","ya"])
-with tab1:
-    st.slider("j",0.0, 5.0,key="x0")
-with tab2:
-    st.slider("j",0.0, 5.0,key="x")

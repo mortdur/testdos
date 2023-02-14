64860c59f52b5e61e3f8e417900422b896c66cd8
mortdur <121159231+mortdur@users.noreply.github.com>
2023-02-14 10:20:17 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 import streamlit as st
-<a href="https://twitter.com/intent/tweet?button_hashtag=LoveTwitter&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #LoveTwitter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<a href="https://twitter.com/intent/tweet?button_hashtag=salud&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #salud</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>

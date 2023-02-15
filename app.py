b831d5ec80aa973eb385288d632d77a9a103c3f3
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-15 10:17:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def TheTweet(tweet_url):
 
 res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")
 #st.write(TheTweet)
-components.html(<blockquote class="twitter-tweet"><p lang="en" dir="ltr">I think about Eric Andre having Tyler The Creator on his show a lot <a href="https://t.co/0IEknST6oM">pic.twitter.com/0IEknST6oM</a></p>&mdash; Blake Garman (@FrostedBlakes34) <a href="https://twitter.com/FrostedBlakes34/status/1625617524564828174?ref_src=twsrc%5Etfw">February 14, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> , height=600)
+components.html(res,height=600)
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">I think about Eric Andre having Tyler The Creator on his show a lot <a href="https://t.co/0IEknST6oM">pic.twitter.com/0IEknST6oM</a></p>&mdash; Blake Garman (@FrostedBlakes34) <a href="https://twitter.com/FrostedBlakes34/status/1625617524564828174?ref_src=twsrc%5Etfw">February 14, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 

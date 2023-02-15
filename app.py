14778f3167ff147558ecf76217f68d0b3636c47b
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-15 10:05:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
+from requests.api import request
+import streamlit as st
 import requests
 import streamlit.components.v1 as components
 
-
-class Tweet(object):
-    def __init__(self, s, embed_str=False):
-        if not embed_str:
-            # Use Twitter's oEmbed API
-            # https://dev.twitter.com/web/embedded-tweets
-            api = "https://publish.twitter.com/oembed?url={}".format(s)
-            response = requests.get(api)
-            self.text = response.json()["html"]
-        else:
-            self.text = s
-
-    def _repr_html_(self):
-        return self.text
-
-    def component(self):
-        return components.html(self.text, height=600)
-
-
-t = Tweet("https://twitter.com/ark9451/status/1579814253070266375").component()
+def TheTweet(tweet_url):
+    api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
+    response = requests.get(api)
+    res = response.json()["html"]
+    return res
+input = "https://twitter.com/ark9451/status/1579814253070266375"
+res = TheTweet(input)
+#st.write(TheTweet)
+components.html(res, height=600)

f44ddf641592e623df6844dea1c709e15ee8c838
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-15 09:55:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import streamlit as st
 import requests
 import streamlit.components.v1 as components
 
-def TheTweet(tweet_url):
-    api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
-    response = requests.get(api)
-    res = response.json()["html"]
-    return res
+class Tweet(object):
+    def __init__(self,tweet_url):
+        api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
+        response = requests.get(api)
+        res = response.json()["html"]
+        return res
 
-res = TheTweet("https://twitter.com/ark9451/status/1579814253070266375")
-#st.write(TheTweet)
-components.html(res, height=600)
+    def ajuste(self):
+        components.html(res, height=600)
+        
+TheTweet("https://twitter.com/ark9451/status/1579814253070266375")

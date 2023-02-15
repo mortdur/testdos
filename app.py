312e1bb3c70c3efefa7d30fc66e831b9a48388ea
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-15 10:19:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 def TheTweet(tweet_url):
     api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
     response = requests.get(api)
-    res = response.json()
+    res = response.json()["html"]
     return res
 
 res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")

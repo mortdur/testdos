43ae3f85bb1d5bf5a8b2d97b0b5dc0d9e85317a9
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-16 12:36:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ def TheTweet(tweet_url):
     res = response.json()["html"]
     return res
 
-res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")
-#st.write(TheTweet)
+col1, col2 = st.columns([3, 1])
+
+col1.subheader("A wide column with a chart")
+col1.res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")
+
+col2.subheader("A narrow column with the data")
+with st.expander("Tumor cerebral"):
+    st.text("Aqui podremos predecir si tenemos un tumor y de que tipo")
+
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
 with st.expander("Tumor cerebral"):

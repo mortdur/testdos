96d7dcda188254cc037d1a6e2b0b732cc167a621
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-16 10:49:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,3 @@ def TheTweet(tweet_url):
 res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")
 #st.write(TheTweet)
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
-
-expander = st.expander("See explanation")
-expander.write(\"\"\"
-    The chart above shows some numbers I picked for you.
-    I rolled actual dice for these, so they're *guaranteed* to
-    be random.
-\"\"\")

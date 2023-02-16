e9a9f8bb8f3915ff7bc44c9d7e60869da5ff2934
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-16 10:44:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ def TheTweet(tweet_url):
 res = TheTweet("https://twitter.com/FrostedBlakes34/status/1625617524564828174")
 #st.write(TheTweet)
 with st.expander("See explanation"):
-    st.write(\"\"\"
+    st.write("
         The chart above shows some numbers I picked for you.
         I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    \"\"\")
+        be random.")
     components.html(res, height=600)
 
 components.html('<a class="twitter-timeline" data-width="250" data-height="450" data-theme="light" href="https://twitter.com/BradleyLJones?ref_src=twsrc%5Etfw">Tweets by BradleyLJones</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>')

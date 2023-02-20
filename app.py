e600068ac05299d9acca416f1631acd1c00aeeee
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-20 10:15:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ def TheTweet(tweet_url):
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("A cat")
    st.image("https://static.streamlit.io/examples/cat.jpg")
+   with st.expander("Tumor cerebral"):
+    st.text("Aqui podremos predecir si tenemos un tumor y de que tipo")
 
 with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+   with st.expander("Cancer de mama"):
+    st.text("Aqui podremos predecir si tenemos cancer de mama y de que tipo")
 with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
-    
+   components.html('<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',height=420,width=350)
